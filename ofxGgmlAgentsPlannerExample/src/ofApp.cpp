f88cde208eedc9dc3f95ae2680fb59bce9445d2b
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2026-05-16 02:21:45 +0200

    Use ImGui in planner example
    Use ImGui in planner example

--- ofxGgmlAgentsPlannerExample/src/ofApp.cpp
+++ ofxGgmlAgentsPlannerExample/src/ofApp.cpp
@@ -2,6 +2,7 @@
 
 void ofApp::setup() {
 	ofSetWindowTitle("ofxGgmlAgents smoke example");
+	gui.setup(nullptr, false);
 	request.goal = "plan a local creative coding assistant";
 	status = ofxGgmlAgentsUtils::describe(request);
 	ofLogNotice("ofxGgmlAgentsPlannerExample") << status;
@@ -9,7 +10,15 @@ void ofApp::setup() {
 
 void ofApp::draw() {
 	ofBackground(18);
-	ofSetColor(240);
-	ofDrawBitmapString("ofxGgmlAgents", 32, 48);
-	ofDrawBitmapString(status, 32, 78);
+	gui.begin();
+	ImGui::SetNextWindowPos(ImVec2(24.0f, 24.0f), ImGuiCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(560.0f, 220.0f), ImGuiCond_Once);
+	if (ImGui::Begin("ofxGgmlAgents Planner Example")) {
+		ImGui::TextUnformatted("Planning Request");
+		ImGui::Separator();
+		ImGui::TextWrapped("%s", status.c_str());
+	}
+	ImGui::End();
+	gui.end();
+	gui.draw();
 }
